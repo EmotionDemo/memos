84bce800cd36fef66812e31786d75ceef86a8b94
李丰华 <lifenghua@u-care.net.cn>
2023-04-17 23:24:38 +0800

    1、修复了状态栏中文本不显示的问题 2、修复了暂存箱中删除某个内容时产生的角标越界的问题 3、修复了"更多"中，顶部统计数据消失的问题

--- lib/pages/collect_page.dart
+++ lib/pages/collect_page.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:memos/utils/toast.dart';
+import 'package:uuid/uuid.dart';
 
 import '../view/delete_card_view.dart';
 
@@ -58,86 +59,95 @@ class _CollectPageState extends State<CollectPage> {
     return list;
   }
 
+  var listItemCount = 0;
+  List<Widget> list = [Container()];
+
+  @override
+  void initState() {
+    super.initState();
+    list = _queryHoldUpBoxData();
+    listItemCount = list.length;
+  }
+
   @override
   Widget build(BuildContext context) {
-    List<Widget> list = _queryHoldUpBoxData().toList();
     return Scaffold(
       appBar: AppBar(
-        title: Text('11'),
+        backgroundColor: Colors.white,
+        title: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            Container(
+              alignment: Alignment.centerLeft,
+              margin: const EdgeInsets.only(left: 5, top: 5, bottom: 5),
+              padding: EdgeInsets.only(top: 20, bottom: 20),
+              child: Text(
+                '暂存箱',
+                style: TextStyle(color: Colors.black, fontSize: 22),
+              ),
+            ),
+            InkWell(
+              child: Container(
+                margin: EdgeInsets.only(top: 5, right: 5, bottom: 5),
+                child: Icon(
+                  Icons.search,
+                  size: 25,
+                  color: Colors.black,
+                ),
+              ),
+              onTap: () {},
+            ),
+          ],
+        ),
         elevation: 0,
       ),
-      backgroundColor: Colors.black12.withOpacity(0.06),
-      body: SingleChildScrollView(
-        child: Column(
-          // mainAxisSize: MainAxisSize.max,
-          children: [
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: [
-                Container(
-                  alignment: Alignment.centerLeft,
-                  margin: const EdgeInsets.only(left: 10, top: 10),
-                  child: Text(
-                    '暂存箱',
-                    style: TextStyle(
-                        color: Colors.black54.withOpacity(0.5), fontSize: 22),
-                  ),
+      backgroundColor: Colors.white,
+      body: Column(
+        children: [
+          Container(
+            height: 30,
+            margin: const EdgeInsets.only(left: 10, right: 10),
+            decoration: BoxDecoration(
+              color: Colors.black12.withOpacity(0.05),
+              borderRadius: BorderRadius.circular(6),
+            ),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: const [
+                Icon(
+                  Icons.info,
+                  color: Colors.greenAccent,
+                  size: 20,
                 ),
-                InkWell(
-                  child: Container(
-                    margin: EdgeInsets.only(top: 10, right: 20, bottom: 10),
-                    child: Icon(
-                      Icons.search,
-                      size: 25,
-                    ),
-                  ),
-                  onTap: () {},
+                SizedBox(
+                  width: 10,
                 ),
+                Text(
+                  '在条目中左滑恢复右滑永久删除',
+                  style: TextStyle(color: Colors.black54),
+                )
               ],
             ),
-            const SizedBox(
-              height: 5,
-            ),
-            Container(
-              // height: 35,
-              // color: Colors.red,
-              margin: const EdgeInsets.only(left: 10, right: 10),
-              decoration: BoxDecoration(
-                color: Colors.black12.withOpacity(0.05),
-                borderRadius: BorderRadius.circular(6),
-              ),
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                // mainAxisSize: MainAxisSize.min,
-                children: const [
-                  Icon(
-                    Icons.info,
-                    color: Colors.greenAccent,
-                    size: 20,
-                  ),
-                  SizedBox(
-                    width: 10,
-                  ),
-                  Text(
-                    '在条目中左滑恢复右滑永久删除',
-                    style: TextStyle(color: Colors.black54),
-                  )
-                ],
-              ),
-            ),
-
-            ListView.builder(
-             physics: const NeverScrollableScrollPhysics(),
+          ),
+          SizedBox(
+            height: 7,
+          ),
+          Expanded(
+            child: ListView.builder(
+              // physics: const NeverScrollableScrollPhysics(),
               shrinkWrap: true,
-              itemCount: list.length,
+              itemCount: listItemCount,
               itemBuilder: (context, index) {
                 final item = list[index];
                 return Dismissible(
-                  key: Key(index.toString()),
+                  key: Key(Uuid().v4().toString()),
                   onDismissed: (DismissDirection direction) {
                     if (direction == DismissDirection.endToStart) {
                       //恢复
                       list.removeAt(index);
+                      setState(() {
+                        listItemCount = list.length;
+                      });
                     } else {
                       //永久删除
                       ToastUtil.showToast(message: direction.name);
@@ -148,8 +158,8 @@ class _CollectPageState extends State<CollectPage> {
                 );
               },
             ),
-          ],
-        ),
+          ),
+        ],
       ),
     );
   }
--- lib/view/note_card_view.dart
+++ lib/view/note_card_view.dart
@@ -27,7 +27,7 @@ class _NoteCardState extends State<NoteCard> {
           clipBehavior: Clip.antiAlias,
           color: Colors.white,
           shadowColor: Colors.grey,
-          elevation: 0,
+          elevation: 2,
           shape: RoundedRectangleBorder(
             borderRadius: BorderRadius.circular(10),
             // side: BorderSide(
