578d27bad6c642a554c67113583761ec7b9066c6
李丰华 <lifenghua@u-care.net.cn>
2023-05-23 23:13:45 +0800

    gogogo，添加笔记录入页

--- lib/main.dart
+++ lib/main.dart
@@ -103,7 +103,7 @@ class _MyAppState extends State<MyApp> {
         statusBarBrightness: Brightness.dark,
         statusBarIconBrightness: Brightness.light,
       );
-      SystemChrome.setSystemUIOverlayStyle(style);
+      // SystemChrome.setSystemUIOverlayStyle(style);
       //高刷
       try {
         FlutterDisplayMode.setHighRefreshRate();
