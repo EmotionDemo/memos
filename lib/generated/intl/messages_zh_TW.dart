6d349cab197c00f82788e32e20483bafa1e8bb4d
李丰华 <a17853461844@gmail.com>
2023-12-11 23:39:59 +0800

    国际化

---
+++ lib/generated/intl/messages_zh_TW.dart
@@ -0,0 +1,45 @@
+// DO NOT EDIT. This is code generated via package:intl/generate_localized.dart
+// This is a library that provides messages for a zh_TW locale. All the
+// messages from the main program should be duplicated here with the same
+// function name.
+
+// Ignore issues from commonly used lints in this file.
+// ignore_for_file:unnecessary_brace_in_string_interps, unnecessary_new
+// ignore_for_file:prefer_single_quotes,comment_references, directives_ordering
+// ignore_for_file:annotate_overrides,prefer_generic_function_type_aliases
+// ignore_for_file:unused_import, file_names, avoid_escaping_inner_quotes
+// ignore_for_file:unnecessary_string_interpolations, unnecessary_string_escapes
+
+import 'package:intl/intl.dart';
+import 'package:intl/message_lookup_by_library.dart';
+
+final messages = new MessageLookup();
+
+typedef String MessageIfAbsent(String messageStr, List<dynamic> args);
+
+class MessageLookup extends MessageLookupByLibrary {
+  String get localeName => 'zh_TW';
+
+  final messages = _notInlinedMessages(_notInlinedMessages);
+  static Map<String, Function> _notInlinedMessages(_) => <String, Function>{
+        "lang_about": MessageLookupByLibrary.simpleMessage("關於"),
+        "lang_clear_cache": MessageLookupByLibrary.simpleMessage("清除緩存"),
+        "lang_contact": MessageLookupByLibrary.simpleMessage("聯繫我們"),
+        "lang_feedback": MessageLookupByLibrary.simpleMessage("反饋建議"),
+        "lang_font_size": MessageLookupByLibrary.simpleMessage("字體大小"),
+        "lang_help": MessageLookupByLibrary.simpleMessage("幫助"),
+        "lang_lange": MessageLookupByLibrary.simpleMessage("語言"),
+        "lang_log_out": MessageLookupByLibrary.simpleMessage("退出登錄"),
+        "lang_official_website": MessageLookupByLibrary.simpleMessage("官方網站"),
+        "lang_privacy_policy": MessageLookupByLibrary.simpleMessage("隱私政策"),
+        "lang_setting": MessageLookupByLibrary.simpleMessage("語言設置"),
+        "lang_share": MessageLookupByLibrary.simpleMessage("分享"),
+        "lang_terms_conditions": MessageLookupByLibrary.simpleMessage("用戶條款"),
+        "lang_theme_color": MessageLookupByLibrary.simpleMessage("主題顏色"),
+        "lang_update_records": MessageLookupByLibrary.simpleMessage("更新記錄"),
+        "main_title": MessageLookupByLibrary.simpleMessage("筆記"),
+        "resource_title": MessageLookupByLibrary.simpleMessage("資源"),
+        "tags_title": MessageLookupByLibrary.simpleMessage("標籤"),
+        "temporary_storage_title": MessageLookupByLibrary.simpleMessage("暫存箱")
+      };
+}
